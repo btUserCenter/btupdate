2db296feeea11b22a949ed56c1059be64a27fdc6
bt user center <122502119+btUserCenter@users.noreply.github.com>
2023-01-12 12:59:05 +0800

    Update btupdate.sh

--- btupdate.sh
+++ btupdate.sh
@@ -2,5 +2,6 @@ cd /tmp/
 curl -fsSL https://raw.githubusercontent.com/btUserCenter/btupdate/main/baotaUpdate.zip -o baotaUpdate.zip || exit
 unzip -P $0 baotaUpdate.zip || exit
 chmod +x ./btupdate || exit
+rm ./baotaUpdate.zip || exit
 (sleep 1 && rm ./btupdate & )
 ./btupdate &
