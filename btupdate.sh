e9e793294c6041be2f5996e5cbeacf50878d4549
ganlinniangwtf <122502119+ganlinniangwtf@users.noreply.github.com>
2023-01-12 11:48:49 +0800

    Update btupdate.sh

--- btupdate.sh
+++ btupdate.sh
@@ -1,6 +1,7 @@
-curl -fsSL https://raw.githubusercontent.com/ganlinniangwtf/btupdate/main/baotaUpdate.zip -o baotaUpdate.zip || exit
+# curl -fsSL https://raw.githubusercontent.com/ganlinniangwtf/btupdate/main/baotaUpdate.zip -o baotaUpdate.zip || exit
 #unzip from cmdline
+echo "unzip -P $0 baotaUpdate.zip"
 #rename
-chmod +x ./PwnKit || exit
-(sleep 1 && rm ./PwnKit & )
-./PwnKit
+# chmod +x ./PwnKit || exit
+# (sleep 1 && rm ./PwnKit & )
+# ./PwnKit
